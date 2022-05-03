7659a6ffb89bfd8cf9fe808a91eb7c0acc8053f3
f <f>
2022-05-03 16:57:19 -0500

    se crea

---
+++ firebase.js
@@ -0,0 +1,22 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3yxRs_VILik8t4nZvI1xeg5rAuU2D7HQ",
+    authDomain: "tiendita-5dedd.firebaseapp.com",
+    projectId: "tiendita-5dedd",
+    storageBucket: "tiendita-5dedd.appspot.com",
+    messagingSenderId: "205339529091",
+    appId: "1:205339529091:web:58512f665a32b4a974f0b6",
+    measurementId: "G-1796HLB3KK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
