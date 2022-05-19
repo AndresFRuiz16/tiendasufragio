980d8720436b3a58623388919cbabcd852d0be79
Andres <felipermko@gmail.com>
2022-05-18 21:50:08 -0500

    solo falta mejorar

--- controlador.js
+++ controlador.js
@@ -28,389 +28,389 @@ let producto3={
 
 let producto4={
     nombre:"Hubba Bubba",
-    precio:500000,
+    precio:2000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/hubba%20bubba.jfif?alt=media&token=a1db99c7-8f3a-4c91-b691-d6a4bfdc0628",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"75%"
 }
 
 let producto5={
     nombre:"oka loka caja",
-    precio:500000,
+    precio:2500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/oka%20loka%20cajita.jfif?alt=media&token=39352a1f-bafc-45d9-a752-fa436ed2cfb0",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"86%"
 }
 
 let producto6={
     nombre:"Oka loka",
-    precio:500000,
+    precio:2300,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/oka%20loka.jfif?alt=media&token=19150d00-d2a7-4417-8e4b-bf92a6ffa312",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"82%"
 }
 
 let producto7={
     nombre:"Super coco",
-    precio:500000,
+    precio:200,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/supercoco.jfif?alt=media&token=7faaadef-7381-4f35-a422-0a177a432a45",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"80%"
 }
 
 let producto8={
     nombre:"Gomitas aro",
-    precio:500000,
+    precio:300,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/gomitas%20aro.jfif?alt=media&token=f15c15a6-9817-436c-8171-89a7f4096265",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"84%"
 }
 
 let producto9={
     nombre:"Gomitas fresa",
-    precio:500000,
+    precio:300,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/gomitas%20fresa.jfif?alt=media&token=924940b1-1637-4178-a6ea-29f13f6b5846",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"85%"
 }
 
 let producto10={
     nombre:"Gomitas gusnaito",
-    precio:500000,
+    precio:300,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/gomitas%20gusanito.jfif?alt=media&token=af180b55-b286-4de0-aeb4-17fc6d3f8277",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"88%"
 }
 
 let producto11={
     nombre:"Bolas de chocolate blanco",
-    precio:500000,
+    precio:1000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/bolitas%20de%20chocolate%20blanco.jfif?alt=media&token=f3845bd0-5664-4204-9bed-61d633f911ff",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"85%"
 }
 
 let producto12={
     nombre:"Bolas de chocolate",
-    precio:500000,
+    precio:1000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/bolitas%20de%20chocolate%20negro.jfif?alt=media&token=e72884df-2ab7-49a4-80d8-ed732c4d5af6",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"86%"
 }
 
 let producto13={
     nombre:"Hersheys",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/hersheys.jfif?alt=media&token=6abdf5f9-c352-4bda-aa9d-8f06420f626b",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"84%"
 }
 
 let producto14={
     nombre:"Jet",
-    precio:500000,
+    precio:600,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/jet.jfif?alt=media&token=d454a589-4696-4365-83e9-2bb324c4a61e",
     pais:"Colombia",
-    popularidad:""}
+    popularidad:"90%"}
 
 let producto15={
     nombre:"Milkyway",
-    precio:500000,
+    precio:3100,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/milkyway.jfif?alt=media&token=1b8c6fea-87d4-4314-a4d7-504062fb95fe",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"83%"
 }
 let producto16={
     nombre:"MyM",
-    precio:500000,
+    precio:3200,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/mym.jfif?alt=media&token=05e1ebdd-0c72-4d53-927d-6691cef1063e",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"81%"
 }
 let producto17={
     nombre:"Ponky nucita",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/ponky%20nucita.jfif?alt=media&token=919783fd-f73f-4bc5-a96c-ecc77d02c562",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"90%"
 }
 let producto18={
     nombre:"Ponky vainilla",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/ponky%20vainilla.jfif?alt=media&token=fbecd7b3-4072-47b0-9f8a-b0eb38d19399",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"87%"
 }
 let producto19={
     nombre:"Snickers",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/snickers.jfif?alt=media&token=6586124b-6831-4f98-ab2f-c89306c2c218",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"85%"
 }
 let producto20={
     nombre:"twix",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/twix.jfif?alt=media&token=4e87391d-321a-42c7-903c-859602928dff",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"82%"
 }
 let producto21={
     nombre:"springles",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/springles.jfif?alt=media&token=6ef8d11e-b768-4dcb-a99a-e301cd47740d",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"81%"
 }
 let producto22={
     nombre:"DeTodito Mix",
-    precio:500000,
+    precio:2800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/detodito%20mix.jfif?alt=media&token=f70d7fd8-6744-44ab-ad3c-8a3c176bf70e",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"75%"
 }
 let producto23={
     nombre:"DeTodito Natural",
-    precio:500000,
+    precio:2800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/detodito%20natural.jfif?alt=media&token=364e0a7e-2bdf-4cb0-9c18-4719010fa38e",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"81%"
 }
 let producto24={
     nombre:"DeTodito Limon",
-    precio:500000,
+    precio:2800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/detodito%20limon.jfif?alt=media&token=cbeec34f-6401-45b4-b0ca-2fab035c1149",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"82%"
 }
 let producto25={
     nombre:"DeTodito BQQ",
-    precio:500000,
+    precio:2800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/detodito%20bbq.jfif?alt=media&token=db49d2d1-ae8d-427d-ad07-a5d853be8f72",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"76%"
 }
 let producto26={
     nombre:"Nerds",
-    precio:500000,
+    precio:2500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/nerds.jpg?alt=media&token=248f318d-1594-4bc1-ac18-2ca12477c6b1",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"82%"
 }
 let producto27={
     nombre:"Festival de Chocolate",
-    precio:500000,
+    precio:1000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/festival%20chocolate.png?alt=media&token=d5395d1e-ac14-4713-9273-936848a427e1",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"79%"
 }
 let producto28={
     nombre:"Chewy Dips",
-    precio:500000,
+    precio:10000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chewy%20dipps.jpeg?alt=media&token=854c38d8-06c8-4061-ba76-f37cb3982920",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"96%"
 }
 let producto29={
     nombre:"Chokis galletas azul",
-    precio:500000,
+    precio:1400,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chokis%20azul.jpg?alt=media&token=aa015be9-1fd6-4497-abdf-712a68a0bdb7",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"89%"
 }
 let producto30={
     nombre:"Chokis bolitas",
-    precio:500000,
+    precio:1200,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chokis%20bola.jpg?alt=media&token=564eb527-926f-4827-9598-22ee6918f2e7",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"87%"
 }
 let producto31={
     nombre:"Sparkies",
-    precio:500000,
+    precio:1500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/sparkies.jpg?alt=media&token=c1a3ffc7-341b-4482-b92f-acc04ca8333d",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"84%"
 }
 let producto32={
     nombre:"Chocoramo",
-    precio:500000,
+    precio:2000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chocoramo.jpg?alt=media&token=28453436-f4b6-4330-bd86-afda2c421e42",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"88%"
 }
 let producto33={
     nombre:"Milo nuggets",
-    precio:500000,
+    precio:800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/Milo-Nuggets.jpg?alt=media&token=27e7a5f1-91fc-403c-bcb0-5e89b44ba7a2",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"83%"
 }
 let producto34={
     nombre:"Takis",
-    precio:500000,
+    precio:2800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/takis.png?alt=media&token=1b3f8d92-ff9f-467b-af8f-4e9ed019f3ce",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"89%"
 }
 let producto35={
     nombre:"Chocomelos",
-    precio:500000,
+    precio:1600,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chocomelos.jpg?alt=media&token=086f7175-af87-40c3-a6ea-21bbedafb053",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"90%"
 }
 let producto36={
     nombre:"Chokis rojo",
-    precio:500000,
+    precio:1400,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chokis%20rojo.jfif?alt=media&token=47c8d891-10b4-4c1c-afd6-6549f79fb50f",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"87%"
 }
 let producto37={
     nombre:"Frunas",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/frunas.jfif?alt=media&token=00d6defc-a9ca-4b84-8f4a-cc777f8fb453",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"78%"
 }
 let producto38={
     nombre:"AirHeads",
-    precio:500000,
+    precio:4000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/air%20heads.jfif?alt=media&token=04967574-d5ce-4ad3-87c7-7bf899de87ad",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"76%"
 }
 let producto39={
     nombre:"Arequipe",
-    precio:500000,
+    precio:3000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/arequipe.jfif?alt=media&token=12e64392-0050-42bf-bd9c-aafd2c8403f2",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"86%"
 }
 let producto40={
     nombre:"Cocada",
-    precio:500000,
+    precio:1000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/cocada.jfif?alt=media&token=32320cdd-e4bc-4f82-87f2-35849dc41ae3",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"81%"
 }
 let producto41={
     nombre:"Galleta de chocolate nonpareils",
-    precio:500000,
+    precio:500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/calleta%20de%20chocolate%20nonpareils.jfif?alt=media&token=5ea3531c-08f6-4cd2-942c-d433f40656a8",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"78%"
 }
 let producto42={
     nombre:"Quipitos",
-    precio:500000,
+    precio:800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/quipitos.jfif?alt=media&token=964426b1-af45-4cc0-9835-df558ed73278",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"80%"
 }
 let producto43={
     nombre:"Gol",
-    precio:500000,
+    precio:1500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/gol.jfif?alt=media&token=8f2e4c90-489c-4ba8-8f55-88cf88a0022a",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"87%"
 }
 let producto44={
     nombre:"Kitkat",
-    precio:500000,
+    precio:2500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/kit%20kat.jfif?alt=media&token=eb69fc9d-b578-4232-a23f-bb588802a7d3",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"83%"
 }
 let producto45={
     nombre:"Chewy Chocolate chip",
-    precio:500000,
+    precio:10000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chewy%20chocolate%20chip.jfif?alt=media&token=9e922ec0-2886-4c73-a7fb-513a18895a73",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"95%"
 }
 let producto46={
     nombre:"Oreo",
-    precio:500000,
+    precio:900,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/oreo.jfif?alt=media&token=73d35d49-bc98-499f-bccb-b2b487882774",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"84%"
 }
 let producto47={
     nombre:"Gol Jet",
-    precio:500000,
+    precio:500,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/gol%20jet.png?alt=media&token=d444dde3-f38f-4e55-b192-668acf58eb8c",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"85%"
 }
 let producto48={
     nombre:"Chocomix",
-    precio:500000,
+    precio:800,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chokomix.jfif?alt=media&token=61d3ec6c-1722-4eab-9464-ba6801f7d943",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"93%"
 }
 let producto49={
     nombre:"Osos de Chocolate",
-    precio:500000,
+    precio:300,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/osos%20de%20chocolates.jfif?alt=media&token=4add2c4a-f18a-4a31-ab37-8e876de60f74",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"90%"
 }
 let producto50={
     nombre:"Chokis negro",
-    precio:500000,
+    precio:1400,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-5dedd.appspot.com/o/chokis%20negro.jfif?alt=media&token=c72ec7f8-9550-4f49-a40f-fe55a841420c",
     pais:"Colombia",
-    popularidad:""
+    popularidad:"88%"
 }
 
 //ARREGLO
